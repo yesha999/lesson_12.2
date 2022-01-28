24dc1b27671c4002fdee4e9627552fab4e92fe20
yesha999 <scrolltrip@mail.ru>
2022-01-28 16:33:46 +0300

    Созданы странички search, skill, main доработан

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 with open('candidates.json', encoding='utf-8') as file:
@@ -36,6 +36,34 @@ def candidate_page(number):
 @app.route('/list/')
 def list_page():
     return render_template("list_page.html", candidates=candidates)
+
+
+@app.route('/search/')
+def search_page():
+    s = request.args.get("name")
+    if s is None:
+        return 'Введите параметр name'
+    if settings['case-sensitive'] is False:
+        s = s.lower()
+        candidates_search = [candidate for candidate in candidates if s in candidate['name'].lower()]
+    else:
+        candidates_search = [candidate for candidate in candidates if s in candidate['name']]
+    if len(candidates_search):
+        return render_template("search_page.html", canidates_count=len(candidates_search),
+                               candidates_search=candidates_search)
+    return "Кандидатов не найдено"
+
+
+@app.route('/skill/<skill>')
+def skill_page(skill):
+    candidates_w_skill = []
+    for candidate in candidates:
+        if skill in candidate['skills']:
+            candidates_w_skill.append(candidate)
+    if len(candidates_w_skill) > int(settings['limit']):
+        candidates_w_skill = candidates_w_skill[0:settings['limit']]
+    return render_template("skills_page.html", candidates_w_skill=candidates_w_skill,
+                           candidates_w_skill_count=len(candidates_w_skill), skill=skill)
 
 
 if __name__ == "__main__":
